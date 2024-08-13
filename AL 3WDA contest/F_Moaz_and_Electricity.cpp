4a17c3359196666d3b3d55cbf7c1d206e359cf89
HamzaTaigo <151782817+HamzaTaigo@users.noreply.github.com>
2024-08-13 03:20:14 +0300

    Update F_Moaz_and_Electricity.cpp

--- AL 3WDA contest/F_Moaz_and_Electricity.cpp
+++ AL 3WDA contest/F_Moaz_and_Electricity.cpp
@@ -22,31 +22,18 @@
 // Oh ALLAH, teach us what will benefit us, benefit us with what you taught us, and increase our knowledge
 //////////////////////////////////////////////////////////////////////////////////////////////
 #include <bits/stdc++.h>
-#define ll long long
-#define all(vec) vec.begin(), vec.end()
-#define rall(vec) vec.rbegin(), vec.rend()
-#define endl '\n'
-#define Hamza_taigo ios_base::sync_with_stdio(0); cin.tie(0), cout.tie(0);
 using namespace std;
-//صلي على سيد الخلق
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> v(n);
+  for (auto &i: v) cin >> i;
+  cout << accumulate(v.begin(), v.end(), 0) << '\n';
+}
 int main() {
-    Hamza_taigo
-    int t;
-    cin >> t; 
- 
-    while (t--) {
-        int n;
-        cin >> n;  
- 
-        int c[n]; 
-        int total_cost = 0;  
- 
-        for (int i = 0; i < n; ++i) {
-            cin >> c[i];  
-            total_cost += c[i];  
-        }
- 
-        cout << total_cost << endl;  
-    }
-
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t; 
+  cin >> t;
+  while(t--) solve();
 }
